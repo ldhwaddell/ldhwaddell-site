6aaa4164965a9d6c4a0cb2ae51c1951c279d4f3d
ldhwaddell <ldhwaddell@gmail.com>
2024-04-24 11:54:59 -0300

    add components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -76,11 +76,13 @@ const config = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require("tailwindcss-animate"),
+    require("@codaworks/react-glow/tailwind"),
+  ],
 } satisfies Config;
 
 export default config;
