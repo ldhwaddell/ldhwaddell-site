8a4979e0de6a109b9ab62b4dac361de35d76fabe
ldhwaddell <ldhwaddell@gmail.com>
2024-04-23 21:40:04 -0300

    general updates

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { NavBar } from "./nav-bar";
 import { ModeToggle } from "./mode-toggle";
+import { TypographyH2 } from "./typography/h2";
 
 const Header = () => {
   return (
     <header className="flex justify-between items-center pt-5 w-3/4 fixed top-0 z-10">
-      <div className="text-2xl font-bold">Lucas Waddell</div>
+      <TypographyH2 text="Lucas Waddell" />
       <div className="flex items-center">
         <NavBar />
         <ModeToggle />
