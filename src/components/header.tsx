cc09ebca69f49e68966eb10f912f86f5e6719ffa
ldhwaddell <ldhwaddell@gmail.com>
2024-04-29 22:35:29 -0300

    update for new navbar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,20 +1,47 @@
-import React from "react";
-import { NavBar } from "./nav-bar";
-import { ModeToggle } from "./mode-toggle";
-import { TypographyH2 } from "./typography/h2";
+import Link from "next/link";
+import { siteConfig } from "@/config/site";
+import { Icons } from "@/components/icons";
+import { Nav } from "./nav";
+import { MobileNav } from "./mobile-nav";
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/mode-toggle";
 
-const Header = () => {
+export function Header() {
   return (
-    <div className="fixed inset-x-0 top-0 z-20 bg-background">
-      <header className="relative mx-auto flex justify-between items-center w-3/4 h-24 bg-background">
-        <TypographyH2 text="Lucas Waddell" />
-        <div className="flex items-center">
-          <NavBar />
-          <ModeToggle />
+    <header className="sticky top-0 z-50 w-full border-b border-border bg-background">
+      <div className="mx-auto w-3/4 max-w-screen-2xl h-14 flex items-center justify-between">
+        <div className="flex grow-0">
+          <Nav />
+          <MobileNav />
         </div>
-      </header>
-    </div>
+        <div className="flex grow-0 items-center space-x-2">
+          <nav className="flex items-center">
+            <Button variant="ghost" size="icon" asChild>
+              <Link
+                href={siteConfig.links.github}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Icons.gitHub className="h-[1.2rem] w-[1.2rem]" />
+                <span className="sr-only">GitHub</span>
+              </Link>
+            </Button>
+            <Button variant="ghost" size="icon" asChild>
+              <Link
+                href={siteConfig.links.linkedin}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Icons.linkedin className="h-[1.2rem] w-[1.2rem]" />
+                <span className="sr-only">LinkedIn</span>
+              </Link>
+            </Button>
+            <ModeToggle />
+          </nav>
+        </div>
+      </div>
+    </header>
   );
-};
+}
 
 export default Header;
